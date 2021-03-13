1ab6fd92fd0d79aa8b999c25a6707396d6224609
Jin Hong <jnhg1016@gmail.com>
2021-03-13 17:36:59 +1100

    adjustments made to variables and arrays

--- Develop/script.js
+++ Develop/script.js
@@ -2,54 +2,52 @@
 var generateBtn = document.querySelector("#generate");
 
 // Assigned Variables
-var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var special = ["!","@","#","$","%","^","&","*","(",")","[","]","{","}",",",".","<",">","?","|","-","_","=","+"];
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialcharactersArray = ["!","@","#","$","%","^","&","*","(",")","[","]","{","}",",",".","<",">","?","|","-","_","=","+"];
 
 // Empty array variables
 var resultArray = [];
 var userArray = [];
 
 // Generate password function gets called in writePassword function, and it should return the final password
-function generatePassword(){
-  // Do random generation here and return the final password in the end
-  var result = "";
+function generatePassword() {
   // Ask the user how many characters they want
-  var length = prompt("How many characters in your random password (between 8 and 128)?");
-  if(isNaN(length)){
+  var numCharacter = prompt ("How many characters in your random password (between 8 and 128)?");
+  
+  if(isNaN(numCharacter)){
     alert("You must input a number!");
     return generatePassword();
   }
-  if(length<8 || length>128){
+  if(numCharacter<8 || numCharacter>128){
     alert("Please choose a number between 8 to 128!");
     return generatePassword();
   }
-  // Ask the user if they want to include upper, lower, numbersand special characters
-  var hasUpper = confirm("Include upper case letters?");
-  var hasLower = confirm("Include lower case letters?");
-  var hasNumbers = confirm("Include numbers?");
-  var hasSpecial = confirm("Include special characters?");
 
-  if (!hasUpper&&!hasLower&&!hasNumbers&&!hasSpecial){
-    alert("You must at least choose 1 character type!");
-    return generatePassword();
-  }
+  // Ask the user if they want to include upper, lower, numbers and special characters
+  var hasUpper = confirm ("Include upper case letters?");
+  var hasLower = confirm ("Include lower case letters?");
+  var hasNumbers = confirm ("Include numbers?");
+  var hasSpecial = confirm ("Include special characters?");
+
+  // Condition of arrays
   if (hasUpper){
-    resultArray = resultArray.concat(upper);
+    resultArray = resultArray.concat(uppercaseArray);
   }
   if (hasLower){
-    resultArray = resultArray.concat(lower);
+    resultArray = resultArray.concat(lowercaseArray);
   }
   if (hasNumbers){
-    resultArray = resultArray.concat(numbers);
+    resultArray = resultArray.concat(numbersArray);
   }
   if (hasSpecial){
-    resultArray = resultArray.concat(special);
+    resultArray = resultArray.concat(specialcharactersArray);
   }
+  
   console.log(resultArray);
 
-  for (var i = 0; i < length; i++) {
+  for (var i = 0; i < numCharacter; i++) {
     userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]);
   }
   return userArray.join("");
