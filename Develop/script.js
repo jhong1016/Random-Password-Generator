694a0364476f95c6b1b1971f8a7225f816c0af93
Jin Hong <jnhg1016@gmail.com>
2021-03-13 16:31:57 +1100

    added assigned variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Assigned Variables
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var special = "!@#$%^&*()[]{},./\<>?|";
+var chosenCharacters = " ";
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
