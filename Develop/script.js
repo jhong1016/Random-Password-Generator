b32f80c64f178ef1a14b2418787589b77ce43219
Jin Hong <jnhg1016@gmail.com>
2021-03-13 16:46:10 +1100

    generate password function gets called in writePassword function to return the final password

--- Develop/script.js
+++ Develop/script.js
@@ -6,8 +6,7 @@ var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var special = "!@#$%^&*()[]{},./\<>?|";
-var chosenCharacters = " ";
-
+var chosenCharacters = "";
 
 // Write password to the #password input
 function writePassword() {
@@ -15,8 +14,19 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+// Generate password function gets called in writePassword function, and it should return the final password
+function generatePassword(){
+  // Do random generation here and return the final password in the end
+  var result = "";
+  // Ask the user how many characters they want
+  var length = prompt("How many characters in your random password (between 8 and 128)?");
+  if(isNaN(length)){
+    alert("You must input a number!");
+    return generatePassword();
+  }
+} 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
